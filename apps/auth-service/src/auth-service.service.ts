b1ce5e471c0fc5a5b01e717dfcdb75da44c59277
hissoune <khalidhissoune962@gmail.com>
2025-02-02 15:56:14 +0100

    Modify auth-service logic

--- apps/auth-service/src/auth-service.service.ts
+++ apps/auth-service/src/auth-service.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
 
 @Injectable()
 export class AuthServiceService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+  constructor(private readonly configService: ConfigService) {}
+  
+
+  
 }
