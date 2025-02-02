1b240acb3fbc2f65811482eab13f71fafd5aac4c
hissoune <khalidhissoune962@gmail.com>
2025-02-02 15:56:14 +0100

    Update auth-service module

--- apps/auth-service/src/auth-service.module.ts
+++ apps/auth-service/src/auth-service.module.ts
@@ -1,9 +1,36 @@
 import { Module } from '@nestjs/common';
 import { AuthServiceController } from './auth-service.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import AuthConfig from './config/config';
 import { AuthServiceService } from './auth-service.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserModelSchema } from './schemas/user.schema';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [AuthConfig], 
+    }),
+    MongooseModule.forFeature([{ name: User.name, schema: UserModelSchema }]),
+
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('app.mongourl'),
+      }),
+    }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('app.jwtSecret'), 
+        signOptions: { expiresIn: '1000h' },
+      })
+    }),
+  ],
   controllers: [AuthServiceController],
   providers: [AuthServiceService],
 })
