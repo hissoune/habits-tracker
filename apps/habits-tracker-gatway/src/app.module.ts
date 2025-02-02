f5a68f792c45f890ac9dcc5f95a629d31f43c6f3
hissoune <khalidhissoune962@gmail.com>
2025-02-02 01:53:30 +0100

    Update app module in habits-tracker gateway

--- apps/habits-tracker-gatway/src/app.module.ts
+++ apps/habits-tracker-gatway/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HabitsModule } from './habits/habits.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [HabitsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
