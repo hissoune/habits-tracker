bdda874f9e53ff4a928cab17c6235e915e1433cc
hissoune <khalidhissoune962@gmail.com>
2025-02-02 23:49:34 +0100

    Updated habits.controller.ts in habits-tracker-gateway

--- apps/habits-tracker-gatway/src/habits/habits.controller.ts
+++ apps/habits-tracker-gatway/src/habits/habits.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { HabitsService } from './habits.service';
+import { AuthguardGuard } from '../authguard/authguard.guard';
 
 @Controller('habits')
 export class HabitsController {
   constructor(private readonly habitsService: HabitsService) {}
+
+  @Get('all')
+  @UseGuards(AuthguardGuard)
+  geteAllHabits(@Req() req){
+    
+    return req.user
+  }
 }
