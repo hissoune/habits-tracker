3197bc8b071b7907b0288782e8bc45227948dfb1
hissoune <khalidhissoune962@gmail.com>
2025-02-02 23:49:34 +0100

    Updated habits.module.ts in habits-tracker-gateway

--- apps/habits-tracker-gatway/src/habits/habits.module.ts
+++ apps/habits-tracker-gatway/src/habits/habits.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { HabitsService } from './habits.service';
 import { HabitsController } from './habits.controller';
+import { AuthModule } from '../auth/auth.module';
+
 
 @Module({
-  
+  imports: [
+    AuthModule
+  ],
   controllers: [HabitsController],
   providers: [HabitsService],
+ 
 })
 export class HabitsModule {}
