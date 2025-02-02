1e4112b48a4010ee2c592aa503e57725678994bd
hissoune <khalidhissoune962@gmail.com>
2025-02-02 01:53:31 +0100

    Add habits directory in habits-tracker gateway

---
+++ apps/habits-tracker-gatway/src/habits/habits.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+import { HabitsService } from './habits.service';
+
+@Controller('habits')
+export class HabitsController {
+  constructor(private readonly habitsService: HabitsService) {}
+}
