7c0e40dac3f1485ad376c35ff734c9c09976a9ae
hissoune <khalidhissoune962@gmail.com>
2025-02-03 00:08:01 +0100

    [MYH-8] fix: Updated habits.controller.ts

--- apps/habits-tracker-gatway/src/habits/habits.controller.ts
+++ apps/habits-tracker-gatway/src/habits/habits.controller.ts
@@ -1,13 +1,17 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { HabitsService } from './habits.service';
 import { AuthguardGuard } from '../authguard/authguard.guard';
+import { Roles } from '../authguard/roles.decorator';
+import { RolesGuard } from '../authguard/roles.guard';
 
 @Controller('habits')
 export class HabitsController {
   constructor(private readonly habitsService: HabitsService) {}
 
   @Get('all')
-  @UseGuards(AuthguardGuard)
+  @UseGuards(AuthguardGuard,RolesGuard)
+  @Roles('client', 'manager')  
+
   geteAllHabits(@Req() req){
     
     return req.user
