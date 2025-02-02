64941fd5e36263dacff97a943830ba8def07ff02
hissoune <khalidhissoune962@gmail.com>
2025-02-02 01:53:30 +0100

    Update auth-service controller

--- apps/auth-service/src/auth-service.controller.ts
+++ apps/auth-service/src/auth-service.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AuthServiceService } from './auth-service.service';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller()
 export class AuthServiceController {
   constructor(private readonly authServiceService: AuthServiceService) {}
 
-  @Get()
-  getHello(): string {
+  @MessagePattern('login')
+  login(): string {
     return this.authServiceService.getHello();
   }
 }
