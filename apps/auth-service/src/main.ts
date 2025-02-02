3840b769c62326934beeace015e7572aa8da1768
hissoune <khalidhissoune962@gmail.com>
2025-02-02 01:53:30 +0100

    Modify main service file

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthServiceModule } from './auth-service.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AuthServiceModule);
-  await app.listen(process.env.port ?? 5000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AuthServiceModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: ["amqp://localhost:5672/"],
+      queue: 'auth_queue',
+      queueOptions: {
+        durable: false,
+      },
+    },
+  });  await app.listen();
 }
 bootstrap();
