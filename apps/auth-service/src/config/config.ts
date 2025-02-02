a0e8446ededa7bc9565fb71ab91b22e3916e044a
hissoune <khalidhissoune962@gmail.com>
2025-02-03 00:45:42 +0100

    [MYH-8] config: Added email configuration for password reset

--- apps/auth-service/src/config/config.ts
+++ apps/auth-service/src/config/config.ts
@@ -3,4 +3,5 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('app', () => ({
    jwtSecret:process.env.JWT_SECRET,
    mongourl:process.env.MONGO_URI_AUTH,
+   appemail:process.env.APP_EMAIL
 }));
