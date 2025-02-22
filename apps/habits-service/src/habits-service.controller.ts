a8f481103a9d0fd9bfa688680d8244dd620cafdf
hissoune <khalidhissoune962@gmail.com>
2025-02-22 13:30:22 +0100

    [Habits] Updated habits service controller

--- apps/habits-service/src/habits-service.controller.ts
+++ apps/habits-service/src/habits-service.controller.ts
@@ -1,12 +1,33 @@
 import { Controller, Get } from '@nestjs/common';
 import { HabitsServiceService } from './habits-service.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { Habit } from './schemas/habit.schema';
 
 @Controller()
 export class HabitsServiceController {
   constructor(private readonly habitsServiceService: HabitsServiceService) {}
 
-  @Get()
-  getHello(): string {
+  @MessagePattern("createHabit")
+  createHabit(@Payload() data:Habit): string {
     return this.habitsServiceService.getHello();
   }
 }
+// @MessagePattern("getHabits")
+// getHabits() {
+//   return this.habitsServiceService.getHabits();
+// }
+
+// @MessagePattern("getHabitById")
+// getHabitById(id: string)g {
+//   return this.habitsServiceService.getHabitById(id);
+// }
+
+// @MessagePattern("updateHabit")
+// updateHabit(data: { id: string, habit: any }): string {
+//   return this.habitsServiceService.updateHabit(data.id, data.habit);
+// }
+
+// @MessagePattern("deleteHabit")
+// deleteHabit(id: string): string {
+//   return this.habitsServiceService.deleteHabit(id);
+// }
