2bc06796aa733dc19b816a4f75ef931aabcb0478
hissoune <khalidhissoune962@gmail.com>
2025-02-22 14:48:04 +0100

    [Habits Gateway] Updated habits controller

--- apps/habits-tracker-gatway/src/habits/habits.controller.ts
+++ apps/habits-tracker-gatway/src/habits/habits.controller.ts
@@ -1,46 +1,38 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Req, Request, UseGuards } from '@nestjs/common';
 import { HabitsService } from './habits.service';
 import { AuthguardGuard } from '../authguard/authguard.guard';
-import { Roles } from '../authguard/roles.decorator';
-import { RolesGuard } from '../authguard/roles.guard';
+
 import { CreateHabitDto } from './dto/create-habit.dto';
 import { UpdateHabitDto } from './dto/update-habit.dto';
 
 @Controller('habits')
+@UseGuards(AuthguardGuard)
 export class HabitsController {
   constructor(private readonly habitsService: HabitsService) {}
 
   @Get('all')
-  @UseGuards(AuthguardGuard, RolesGuard)
-  @Roles('client', 'manager')  
-  getAllHabits() {
+    getAllHabits() {
     return this.habitsService.getAll();
   }
 
   @Get(':id')
-  @UseGuards(AuthguardGuard, RolesGuard)
-  @Roles('client', 'manager')
   getHabit(@Param('id') id: string) {
     return this.habitsService.getOne(id);
   }
 
   @Post()
-  @UseGuards(AuthguardGuard, RolesGuard)
-  @Roles('manager')
-  createHabit(@Body() createHabitDto: CreateHabitDto) {
+  createHabit(@Request() req,@Body() createHabitDto: CreateHabitDto) {
+    const user = req.user
+    createHabitDto.userId = user.id
     return this.habitsService.create(createHabitDto);
   }
 
   @Put(':id')
-  @UseGuards(AuthguardGuard, RolesGuard)
-  @Roles('manager')
   updateHabit(@Param('id') id: string, @Body() updateHabitDto: UpdateHabitDto) {
     return this.habitsService.update(id, updateHabitDto);
   }
 
   @Delete(':id')
-  @UseGuards(AuthguardGuard, RolesGuard)
-  @Roles('manager')
   deleteHabit(@Param('id') id: string) {
     return this.habitsService.delete(id);
   }
