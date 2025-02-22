f74a9b2a84bfb9f190ed55356d05360afd209c95
hissoune <khalidhissoune962@gmail.com>
2025-02-22 13:30:22 +0100

    [Habits] Updated habits service module

--- apps/habits-service/src/habits-service.module.ts
+++ apps/habits-service/src/habits-service.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { HabitsServiceController } from './habits-service.controller';
 import { HabitsServiceService } from './habits-service.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import  habitConfig  from './config/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import {  HabitModel, HabitSchema } from './schemas/habit.schema';
 
 @Module({
-  imports: [],
+  imports: [
+     ConfigModule.forRoot({
+          isGlobal: true,
+          load: [habitConfig], 
+        }),
+        MongooseModule.forFeature([{ name: HabitModel.name, schema: HabitSchema }]),
+    
+        MongooseModule.forRootAsync({
+          imports: [ConfigModule],
+          inject: [ConfigService],
+          useFactory: (configService: ConfigService) => ({
+            uri: configService.get<string>('app.mongourl'),
+           
+          }),
+        }),
+  ],
   controllers: [HabitsServiceController],
   providers: [HabitsServiceService],
 })
