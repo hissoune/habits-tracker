078156efc9f4ec0de225d5b6bc6192b1e104f517
hissoune <khalidhissoune962@gmail.com>
2025-02-22 14:48:02 +0100

    [Habits] Updated habits service service

--- apps/habits-service/src/habits-service.service.ts
+++ apps/habits-service/src/habits-service.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { HabitImplementations } from './implimentations/habit.implimentations';
 
 @Injectable()
 export class HabitsServiceService {
-  getHello(): string {
-    return 'Hello World!';
+    constructor(private readonly habitsImplimentations: HabitImplementations) {}
+  
+  createHabit(habit) {
+    return this.habitsImplimentations.createHabit(habit);
   }
 }
