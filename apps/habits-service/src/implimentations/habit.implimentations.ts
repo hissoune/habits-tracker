dd4e8ab69d34b8f0357402ded996d89b728cdaed
hissoune <khalidhissoune962@gmail.com>
2025-02-24 09:54:50 +0000

    [Habits] Updated habit implementations and remove the habitProgress fns

--- apps/habits-service/src/implimentations/habit.implimentations.ts
+++ apps/habits-service/src/implimentations/habit.implimentations.ts
@@ -1,8 +1,8 @@
 import { InjectModel } from '@nestjs/mongoose';
 import { HabitInterface } from '../interfaces/habit.interface';
 import {  Habit } from '../schemas/habit.schema';
-import { Model } from 'mongoose';
-import { HabitProgress } from '../habitsProgress/habitProgress.schema';
+import mongoose, { Model } from 'mongoose';
+import { HabitProgress } from '../habitsProgress/schemas/habitProgress.schema';
 
 export class HabitImplementations implements HabitInterface {
   
@@ -32,52 +32,17 @@ export class HabitImplementations implements HabitInterface {
       return this.habitModel.findByIdAndDelete(id)
     }
 
-    async getAllHabits(): Promise<Habit[]> {
-        return this.habitModel.find()
+    async getAllHabits(userId:string): Promise<Habit[]> {
+        return this.habitModel.find({userId})
     }
     
     async getHabitsByFrequency(frequency: string): Promise<Habit[]> {
         return this.habitModel.find({ frequency ,status:'active' });
     }
 
-    async getProgress(habitId: string, userId: string): Promise<HabitProgress | null> {
-        return this.habitProgressModel.findOne({ habitId, userId });
-    }
+  
 
-    async createProgress(habitId: string, userId: string, streak: number): Promise<HabitProgress> {
 
-        const progress = new this.habitProgressModel({
-          habitId,
-          userId,
-          date:Date.now(),
-          streak,
-          lastUpdated: new Date(),
-        });
-      
-        return progress.save();
-      }
-
-    async updateProgress(habitId: string, userId: string): Promise<HabitProgress | null> {
-        const habit =await this.habitModel.findById(habitId)
-
-        const progress = await this.habitProgressModel.findOne({ habitId, userId });
-        if ( habit.repeats == progress.streak) {
-            return this.habitModel.findByIdAndUpdate(habitId, {$set:{status:"completed"}})
-        }
-        if (progress.status) {
-           return this.habitProgressModel.findOneAndUpdate(
-          { habitId, userId },
-          { $inc: { streak: 1 }, $set: { lastUpdated: new Date() ,status:false} },
-          { new: true }
-        );
-        }
-        return this.habitProgressModel.findOneAndUpdate(
-            { habitId, userId },
-            {  $set: { lastUpdated: new Date(),streak:0 } },
-            { new: true }
-          );
-       
-      }
 }
     
    
