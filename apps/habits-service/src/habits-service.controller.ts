ac76363d917536c7aa155021e39ad8ad05b79f81
hissoune <khalidhissoune962@gmail.com>
2025-02-24 09:54:46 +0000

    [Habits] Update the habit servce Controller and add the cron there and call the update progress

--- apps/habits-service/src/habits-service.controller.ts
+++ apps/habits-service/src/habits-service.controller.ts
@@ -2,22 +2,46 @@ import { Controller, Get } from '@nestjs/common';
 import { HabitsServiceService } from './habits-service.service';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { Habit } from './schemas/habit.schema';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
-@Controller()
+@Controller() //ask about if that necesary
 export class HabitsServiceController {
   constructor(private readonly habitsServiceService: HabitsServiceService) {}
 
   @MessagePattern("create_habit")
   createHabit(@Payload() data:Habit) {
-  console.log(data);
   
     return this.habitsServiceService.createHabit(data);
   }
+  @MessagePattern("get_all_habits")
+    getHabits(@Payload() data:{userId:string}) {
+      
+      return this.habitsServiceService.getHabits(data.userId);
+    }
+    // @MessagePattern('compleetProgress')
+    // compleeteProgress(@Payload() data:{id:string,userId:string}){
+    //     return this.habitProgressService.compleeteProgress(data.id,data.userId)
+    // }
+
+    @Cron(CronExpression.EVERY_10_SECONDS, { name: 'dailyHabitProgress' })
+  async handleDailyHabits() {
+    console.log('Updating daily habit progress...');
+    await this.habitsServiceService.updateProgressByFrequency('daily');
+  }
+
+  @Cron(CronExpression.EVERY_WEEK, { name: 'weeklyHabitProgress' })
+  async handleWeeklyHabits() {
+    console.log('Updating weekly habit progress...');
+    await this.habitsServiceService.updateProgressByFrequency('weekly');
+  }
+
+  @Cron(CronExpression.EVERY_1ST_DAY_OF_MONTH_AT_MIDNIGHT, { name: 'monthlyHabitProgress' })
+  async handleMonthlyHabits() {
+    console.log('Updating monthly habit progress...');
+    await this.habitsServiceService.updateProgressByFrequency('monthly');
+  }
 }
-// @MessagePattern("getHabits")
-// getHabits() {
-//   return this.habitsServiceService.getHabits();
-// }
+
 
 // @MessagePattern("getHabitById")
 // getHabitById(id: string)g {
