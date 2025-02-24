edbe97efe57237d497a562059befb4d5b540bd8a
hissoune <khalidhissoune962@gmail.com>
2025-02-24 09:54:48 +0000

    [Habits] add the update progress fn in the habit service service for best tracking for the habit

--- apps/habits-service/src/habits-service.service.ts
+++ apps/habits-service/src/habits-service.service.ts
@@ -2,51 +2,38 @@ import { Injectable } from '@nestjs/common';
 import { HabitImplementations } from './implimentations/habit.implimentations';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { Habit } from './schemas/habit.schema';
+import { HabitProgressService } from './habitsProgress/habitProgress.service';
 
 @Injectable()
 export class HabitsServiceService {
  
-    constructor(private readonly habitsImplimentations: HabitImplementations) {}
+    constructor(private readonly habitsImplimentations: HabitImplementations,private readonly habitProgressService:HabitProgressService) {}
   
   createHabit(habit) {
     return this.habitsImplimentations.createHabit(habit);
   }
-
-  @Cron(CronExpression.EVERY_10_SECONDS, { name: 'dailyHabitProgress' })
-  async handleDailyHabits() {
-    console.log('Updating daily habit progress...');
-    await this.updateProgressByFrequency('daily');
+  getHabits(userId:string){
+    return this.habitsImplimentations.getAllHabits(userId);
   }
 
-  @Cron(CronExpression.EVERY_WEEK, { name: 'weeklyHabitProgress' })
-  async handleWeeklyHabits() {
-    console.log('Updating weekly habit progress...');
-    await this.updateProgressByFrequency('weekly');
-  }
-
-  @Cron(CronExpression.EVERY_1ST_DAY_OF_MONTH_AT_MIDNIGHT, { name: 'monthlyHabitProgress' })
-  async handleMonthlyHabits() {
-    console.log('Updating monthly habit progress...');
-    await this.updateProgressByFrequency('monthly');
-  }
+  
 
 
 
-  private async updateProgressByFrequency(frequency: string) {
+   async updateProgressByFrequency(frequency: string) {
     const habits: Habit[] = await this.habitsImplimentations.getHabitsByFrequency(frequency);
-console.log(habits);
 
     for (const habit of habits) {
       const userId = habit.userId;
 
-      let progress = await this.habitsImplimentations.getProgress(habit._id as unknown as string, userId as unknown as string);
+      let progress = await this.habitProgressService.getProgress(habit._id as unknown as string, userId as unknown as string);
 
       if (!progress) {
-        await this.habitsImplimentations.createProgress(habit._id as unknown as string, userId as unknown as string, 1);
+        await this.habitProgressService.createProgress(habit._id as unknown as string, userId as unknown as string, 1);
         console.log(`Progress created for habit: ${habit.title} (${frequency})`);
       } else {
        
-        await this.habitsImplimentations.updateProgress(habit._id as unknown as string, userId as unknown as string);
+        await this.habitProgressService.updateProgress(habit._id as unknown as string, userId as unknown as string);
         console.log(`Progress updated for habit: ${habit.title} (${frequency})`);
       }
     }
