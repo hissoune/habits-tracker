07d4aafa12faa3d8f837e56ec3f9c5fa0fce8f77
hissoune <khalidhissoune962@gmail.com>
2025-02-27 18:52:32 +0000

    Updated habits service controller to align with progress tracking changes and use the enpoints decorators http directly in the habits service

--- apps/habits-service/src/habits-service.controller.ts
+++ apps/habits-service/src/habits-service.controller.ts
@@ -1,52 +1,74 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Put, Req, UseGuards } from '@nestjs/common';
 import { HabitsServiceService } from './habits-service.service';
-import { MessagePattern, Payload } from '@nestjs/microservices';
-import { Habit } from './schemas/habit.schema';
+import { AuthguardGuard } from './authguard/authguard.guard';
+import { CreateHabitDto } from './dto/create-habit.dto';
 import { Cron, CronExpression } from '@nestjs/schedule';
 
-@Controller() //ask about if that necesary
-export class HabitsServiceController {
-  constructor(private readonly habitsServiceService: HabitsServiceService) {}
+@Controller('habits') 
+export class HabitsController {
 
-  @MessagePattern("create_habit")
-  createHabit(@Payload() data:Habit) {
-  
-    return this.habitsServiceService.createHabit(data);
+  constructor(private readonly habitsService: HabitsServiceService) {}
+
+  @Get('health')
+    checkHealth() {
+    return 'UP'
   }
-  @MessagePattern("get_all_habits")
-    getHabits(@Payload() data:{userId:string}) {
+ 
+  @Get('/all')
+  @UseGuards(AuthguardGuard)
+      getAllHabits(@Req() req) {        
+        const user = req.user
+      const userId = user.id
       
-      return this.habitsServiceService.getHabits(data.userId);
+      
+      return this.habitsService.getHabits(userId);
     }
-
-    @MessagePattern("get_habit")
-    getHabitById(@Payload() data: any) {
-    return this.habitsServiceService.getHabitById(data.id);
-  }
-    @MessagePattern('reactive_habit')
-    reactiveHabit(@Payload() data:{id:string}){
-        return this.habitsServiceService.reactiveHabit(data.id)
+  
+    @Get(':id')
+    @UseGuards(AuthguardGuard)
+    getHabit(@Param('id') id: string) {
+      return this.habitsService.getHabitById(id);
     }
+  
+    @Post()
+    @UseGuards(AuthguardGuard)
+    createHabit(@Req() req,@Body() createHabitDto: CreateHabitDto) {
+      const user = req.user
+      createHabitDto.userId = user.id
+      return this.habitsService.createHabit(createHabitDto);
+    }
+  
+    // @Put(':id')
+    // updateHabit(@Param('id') id: string, @Body() updateHabitDto: UpdateHabitDto) {
+    //   return this.habitsService.update(id, updateHabitDto);
+    // }
 
+    @Patch('/:habitId')
+    reactiveHabit(@Param('habitId') habitId: string) {
+      return this.habitsService.reactiveHabit(habitId);
+    }
     @Cron(CronExpression.EVERY_10_SECONDS, { name: 'dailyHabitProgress' })
-  async handleDailyHabits() {
-    console.log('Updating daily habit progress...');
-    await this.habitsServiceService.updateProgressByFrequency('daily');
-  }
-
-  @Cron(CronExpression.EVERY_WEEK, { name: 'weeklyHabitProgress' })
-  async handleWeeklyHabits() {
-    console.log('Updating weekly habit progress...');
-    await this.habitsServiceService.updateProgressByFrequency('weekly');
-  }
-
-  @Cron(CronExpression.EVERY_1ST_DAY_OF_MONTH_AT_MIDNIGHT, { name: 'monthlyHabitProgress' })
-  async handleMonthlyHabits() {
-    console.log('Updating monthly habit progress...');
-    await this.habitsServiceService.updateProgressByFrequency('monthly');
-  }
-
-
+    async handleDailyHabits() {
+      console.log('Updating daily habit progress...');
+      await this.habitsService.updateProgressByFrequency('daily');
+    }
+  
+    @Cron(CronExpression.EVERY_WEEK, { name: 'weeklyHabitProgress' })
+    async handleWeeklyHabits() {
+      console.log('Updating weekly habit progress...');
+      await this.habitsService.updateProgressByFrequency('weekly');
+    }
+  
+    @Cron(CronExpression.EVERY_1ST_DAY_OF_MONTH_AT_MIDNIGHT, { name: 'monthlyHabitProgress' })
+    async handleMonthlyHabits() {
+      console.log('Updating monthly habit progress...');
+      await this.habitsService.updateProgressByFrequency('monthly');
+    }
+  
+    // @Delete(':id')
+    // deleteHabit(@Param('id') id: string) {
+    //   return this.habitsService.delete(id);
+    // }
 }
 
 
