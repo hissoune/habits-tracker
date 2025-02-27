d355725ff4426fc00e6a14b34d08392204ba0680
hissoune <khalidhissoune962@gmail.com>
2025-02-27 18:52:52 +0000

    Added DTOs for habits service for the habit validation

---
+++ apps/habits-service/src/dto/create-habit.dto.ts
@@ -0,0 +1,15 @@
+import { Schema } from 'mongoose';
+import { Frequency, Habit, Status } from '../../types';
+export class CreateHabitDto implements Habit {
+    userId?: Schema.Types.ObjectId;
+    startDate: Date;
+    endDate?: Date;
+    progress?: number;
+    title: string;
+    description?: string;
+    frequency: Frequency;
+    reminderTime?: Date;
+    status: Status;
+    createdAt: Date;
+    updatedAt: Date;
+}
