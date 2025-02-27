8a9504559edf29c113bd301aadb8ee6474ea9b92
hissoune <khalidhissoune962@gmail.com>
2025-02-27 18:52:52 +0000

    Added new habit progress schema and move the habitprogress schema to it

---
+++ apps/habits-service/src/schemas/habitProgress.schema.ts
@@ -0,0 +1,27 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+import * as mongoose from 'mongoose';
+
+
+@Schema({ timestamps: true })
+export class HabitProgress extends Document {
+  
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Habit', required: true })
+  habitId: string;
+  @Prop({ type: mongoose.Schema.Types.ObjectId, required: true })
+  userId: string;
+
+  @Prop({ required: true })
+  date: Date;
+
+  @Prop({ required: true, default: false })
+  status: boolean;
+  @Prop({required:true,enum:['active',"expired"],default:'active'})
+  progressStatus:string
+
+
+  @Prop({ default: 0 })
+  streak: number; 
+}
+
+export const HabitProgressSchema = SchemaFactory.createForClass(HabitProgress);
