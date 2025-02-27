283501d0308aabac9b60d82a4a1b226964e8679d
hissoune <khalidhissoune962@gmail.com>
2025-02-27 18:52:35 +0000

    Deleted habit progress interface as it's no longer needed

--- apps/habits-service/src/habitsProgress/interfaces/habitProgress.interface.ts
+++
@@ -1,8 +0,0 @@
-import { HabitProgress } from "../schemas/habitProgress.schema"
-
-
-export interface HabitProgressInterface {
-     getProgress(habitId: string, userId: string): Promise<HabitProgress | null> 
-     createProgress(habitId: string, userId: string, streak: number): Promise<HabitProgress> 
-     updateProgress(habitId: string, userId: string): Promise<HabitProgress | null>
-}
