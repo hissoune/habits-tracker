e089d4885d4abbf94963f4f9461cb313f39d9813
hissoune <khalidhissoune962@gmail.com>
2025-02-27 18:52:38 +0000

    Updated app controller for habit tracker Gateway to handel all the requests and dispatch to the other srvices

--- apps/habits-tracker-gatway/src/app.controller.ts
+++ apps/habits-tracker-gatway/src/app.controller.ts
@@ -1,9 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import { All, Controller, Get, Req, Res } from '@nestjs/common';
 import { AppService } from './app.service';
-
+import axios from 'axios';
+const Consul = require('consul');
 @Controller()
 export class AppController {
+  private consul = new Consul();
+
   constructor(private readonly appService: AppService) {}
+
+  async getServiceAddress(serviceName: string): Promise<string | null> {
+    const services = await this.consul.agent.service.list();
+    const service = Object.values(services).find((s: any) => s.Service === serviceName) as { Address: string, Port: number } | undefined;
+    return service ? `http://${service.Address}:${service.Port}` : null;
+  }
+
+  @All('*')
+  async proxyRequest(@Req() req, @Res() res) {    
+    const [_, serviceName, ...path] = req.url.split('/');
+    const serviceUrl = await this.getServiceAddress(serviceName);
+
+    if (!serviceUrl) {
+      return res.status(500).json({ error: `Service ${serviceName} not found` });
+    }
+    try {
+      const response = await axios({
+        method: req.method,
+        url: `${serviceUrl}/${path.join('/')}`,
+        data: req.body,
+        headers: req.headers,
+      });
+
+      res.status(response.status).json(response.data);
+    } catch (error) {
+      res.status(error.response?.status || 500).json(error.response?.data || { error: 'Internal Server Error' });
+    }
+  }
+
 
   @Get()
   getHello(): string {
