8af1fac7b0daaea77b72b613195e7ebcfd882247
hissoune <khalidhissoune962@gmail.com>
2025-02-28 03:32:23 +0000

    Add progress tracking logic to chalenges.service.ts for habit completion

--- apps/chalenges/src/chalenges.service.ts
+++ apps/chalenges/src/chalenges.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { ChalengeServiceImplimentation } from './buisness/impl/chalenge-service.impl';
 import { Challenge } from './schemas/chalenge.schema';
 import { CreateChalengeDto } from './dto/create-chalenge.dto';
+import { UpdateChalengeDto } from './dto/update-chalenge.dto';
 
 @Injectable()
 export class ChalengesService {
@@ -10,5 +11,9 @@ export class ChalengesService {
 
   createChalenge(chalenge:CreateChalengeDto):Promise<Challenge> {
     return this.chalengeServiceImplimentation.createChalenge(chalenge);
+  }
+
+  updateChalenge(id:string,chalenge:UpdateChalengeDto):Promise<Challenge>{
+   return this.chalengeServiceImplimentation.updateChalenge(id, chalenge)
   }
 }
