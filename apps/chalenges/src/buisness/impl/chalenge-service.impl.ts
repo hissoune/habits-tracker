470729ae435ad759355b6f784e2e8709320566eb
hissoune <khalidhissoune962@gmail.com>
2025-02-28 03:32:23 +0000

    Implement participant merging logic in chalenge-service.impl.ts to add new participants with progress tracking

--- apps/chalenges/src/buisness/impl/chalenge-service.impl.ts
+++ apps/chalenges/src/buisness/impl/chalenge-service.impl.ts
@@ -1,7 +1,8 @@
 import { CreateChalengeDto } from "../../dto/create-chalenge.dto";
+import { UpdateChalengeDto } from "../../dto/update-chalenge.dto";
 import { Challenge } from "../../schemas/chalenge.schema";
 import { chalengeService } from "../chalenges-service";
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 
@@ -10,10 +11,31 @@ export class ChalengeServiceImplimentation implements chalengeService {
     constructor(
         @InjectModel(Challenge.name) private readonly challengeModel: Model<Challenge>
     ) {}
+    
 
     async createChalenge(chalenge: CreateChalengeDto): Promise<Challenge> {
         const createdChallenge = new this.challengeModel(chalenge);
         return createdChallenge.save();
+    }
+
+    async updateChalenge(id: string, chalenge: UpdateChalengeDto): Promise<Challenge> {
+        const chalengeExist = await  this.challengeModel.findById(id);
+        if(!chalengeExist || chalengeExist.creator != chalenge.creator ){
+            throw new UnauthorizedException('the chalenge doesnt exist or you dont have the permition ')
+        }
+        if (chalenge.participants) {
+            const existingParticipantIds = chalengeExist.participants.map((participant) => participant.userId);
+                const newParticipants = (chalenge.participants as unknown as string[])
+                .filter((newUserId) => !existingParticipantIds.includes(newUserId))
+                .map((newUserId) => ({ userId: newUserId, progress: 0 })); 
+    
+            chalenge.participants = [
+                ...chalengeExist.participants,
+                ...newParticipants 
+            ];
+        }    
+         
+        return await this.challengeModel.findByIdAndUpdate(id, chalenge, { new: true }).exec();
     }
 }
 
