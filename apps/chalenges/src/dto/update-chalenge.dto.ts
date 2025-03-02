1295b110aabc9b67fddfd103ce168eb257137eec
hissoune <khalidhissoune962@gmail.com>
2025-03-02 23:00:09 +0000

    [REMOVE] Delete unused update-chalenge.dto.ts

--- apps/chalenges/src/dto/update-chalenge.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateChalengeDto } from './create-chalenge.dto';
-
-export class UpdateChalengeDto extends PartialType(CreateChalengeDto) {}
