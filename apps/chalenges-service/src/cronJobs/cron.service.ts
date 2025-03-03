6a4fdfa460ca519abf707d57bfce71cb4c08c177
hissoune <khalidhissoune962@gmail.com>
2025-03-03 15:41:33 +0000

    [ADD] Add cronJobs directory for challenge-related cron jobs

---
+++ apps/chalenges-service/src/cronJobs/cron.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@nestjs/common";
+import { ChalengesService } from "../chalenges.service";
+import { Cron, CronExpression } from "@nestjs/schedule";
+
+
+@Injectable()
+export class CronService {
+constructor(private readonly chalengeService:ChalengesService){}
+
+@Cron(CronExpression.EVERY_10_SECONDS, { name: 'dailychalengeProgress' })
+  async handleDailyHabits() {
+    console.log('Updating daily chalenge progress...');
+    await this.chalengeService.updateChalengesProgressByFrequency('daily');
+  }
+
+  @Cron(CronExpression.EVERY_WEEK, { name: 'weeklychalengeProgress' })
+  async handleWeeklyHabits() {
+    console.log('Updating weekly chalenge progress...');
+    await this.chalengeService.updateChalengesProgressByFrequency('weekly');
+  }
+
+  @Cron(CronExpression.EVERY_1ST_DAY_OF_MONTH_AT_MIDNIGHT, { name: 'monthlychalengeProgress' })
+  async handleMonthlyHabits() {
+    console.log('Updating monthly chalenge progress...');
+    await this.chalengeService.updateChalengesProgressByFrequency('monthly');
+  }
+
+}
