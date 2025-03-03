47fc5c96282352a73cb3a2bfddaa0feb4e7553e8
hissoune <khalidhissoune962@gmail.com>
2025-03-03 14:19:24 +0000

    [UPDATE] Update progress-service.ts for progress tracking business logic

--- apps/chalenges-service/src/buisness/progress-service.ts
+++ apps/chalenges-service/src/buisness/progress-service.ts
@@ -5,6 +5,7 @@ import { Progress } from "../schemas/progress.schema";
 export interface progressService {
 
     createProgress(userId:string,chalengeId:string):Promise<Progress>;
+    getProgressForChalenge(userId:string,chalengeId:string):Promise<Progress>;
     completeProgress(id:string,userId:string):Promise<Progress>;
     updateProgress(userId:string,chalengeId:string):Promise<Progress>
 }
