8e8c840d182227acad0296f7a0e6d67d5a0ec1fc
hissoune <khalidhissoune962@gmail.com>
2025-03-03 09:57:13 +0000

    [ADD] Add chalengesProgress module for progress tracking

---
+++ apps/chalenges-service/src/chalengesProgress/progress.module.ts
@@ -0,0 +1,37 @@
+import { Module } from "@nestjs/common";
+import { ProgressService } from "./progress.service";
+import { ProgressController } from "./progress.controller";
+import { AuthguardGuard } from "../authguard/authguard.guard";
+import { ConfigModule } from "@nestjs/config";
+import chalengeConfig from '../config/config'
+import { ClientsModule, Transport } from "@nestjs/microservices";
+
+
+@Module({
+    imports:[
+        ConfigModule.forRoot({
+              isGlobal: true,
+              load: [chalengeConfig], 
+            }),
+            ClientsModule.register([
+                              {
+                                name:"AUTH_SERVICE",
+                                transport:Transport.RMQ,
+                                options:{
+                                  urls:["amqp://localhost:5672/"],
+                                  queue:"auth_queue",
+                                  queueOptions: {
+                                    durable: false,
+                                  },
+                                }
+                              }
+                            ]),
+    ],
+    controllers: [
+        ProgressController
+    ],
+    providers: [
+        ProgressService,AuthguardGuard
+    ],
+})
+export class ProgressModule {}
