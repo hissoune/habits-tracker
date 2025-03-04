b812a3c31e4bedf89144c594775d6031c9a04277
hissoune <khalidhissoune962@gmail.com>
2025-03-04 23:51:24 +0000

    [REMOVE] Remove unused authguard.guard.spec.ts from chalenges-service

--- apps/chalenges-service/src/authguard/authguard.guard.spec.ts
+++
@@ -1,7 +0,0 @@
-import { AuthguardGuard } from './authguard.guard';
-
-describe('AuthguardGuard', () => {
-  it('should be defined', () => {
-    expect(new AuthguardGuard()).toBeDefined();
-  });
-});
