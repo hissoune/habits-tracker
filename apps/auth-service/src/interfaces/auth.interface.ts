ed6dac0029898449107da1c895645f8f4098578c
hissoune <khalidhissoune962@gmail.com>
2025-03-06 19:03:25 +0000

    [UPDATE] Update auth.interface.ts for authentication interface improvements

--- apps/auth-service/src/interfaces/auth.interface.ts
+++ apps/auth-service/src/interfaces/auth.interface.ts
@@ -4,6 +4,8 @@ import { User } from "../schemas/user.schema";
 export interface AuthInterface {
     register(user: User): Promise<User>;
 
+    getUsersByIds(ids:string[]): Promise<User[]>
+    getUserById(userId:string): Promise<User>
     login(user: User): Promise<{ token: string }>;
 
     forgotPassword(email: string): Promise<{ email: string }>;
