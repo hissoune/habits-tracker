7c9ab966d3942ec60248ce4fd9c72974f7eab8af
hissoune <khalidhissoune962@gmail.com>
2025-03-07 15:41:45 +0000

    Updated chalenges.service.ts for progress tracking

--- apps/chalenges-service/src/chalenges.service.ts
+++ apps/chalenges-service/src/chalenges.service.ts
@@ -4,70 +4,100 @@ import { Challenge } from './schemas/chalenge.schema';
 import { CreateChalengeDto } from './dto/create-chalenge.dto';
 import { UpdateChalengeDto } from './dto/update-chalenge.dto';
 import { ClientProxy } from '@nestjs/microservices';
-import { from } from 'rxjs';
-import { log } from 'console';
+import { ProgressService } from './chalengesProgress/progress.service';
+
 
 @Injectable()
 export class ChalengesService {
-  constructor(private readonly chalengeServiceImplimentation:ChalengeServiceImplimentation,@Inject("AUTH_SERVICE") private readonly authClient:ClientProxy){}
+  constructor(private readonly chalengeServiceImplimentation:ChalengeServiceImplimentation,private readonly progressService:ProgressService,@Inject("AUTH_SERVICE") private readonly authClient:ClientProxy){}
+
+  async getCreatrorAndParticipants(challenge:any){
+    let chalenge = challenge.toObject()
+    const creator = await this.authClient.send('get-creator', chalenge.creator).toPromise();
+   
+    if (chalenge.participants && chalenge.participants.length > 0) {
+        const userIds = chalenge.participants.map(p => p.userId);
+        const users = await this.authClient.send('get-users', userIds).toPromise();
+        const updatedParticipants = chalenge.participants.map(participant => {
+            const user = users.find(u => u._id.toString() === participant.userId.toString());
+            return {
+                ...participant,
+                userDetails: user || null, 
+            };
+        });
+
+        chalenge.participants = updatedParticipants;    
+    }
+    chalenge.creator = creator
+
+    return chalenge;
+
+  }
+
 
   async  getAllChalenges(){
 
     const chalenges =await  this.chalengeServiceImplimentation.getAllChalenges();
 
     for (let i = 0; i < chalenges.length; i++) {
-      let chalenge = chalenges[i].toObject(); 
-      const creator = await this.authClient.send('get-creator', chalenge.creator).toPromise();
-   
-      if (chalenge.participants && chalenge.participants.length > 0) {
-          const userIds = chalenge.participants.map(p => p.userId);
-          const users = await this.authClient.send('get-users', userIds).toPromise();
-          const updatedParticipants = chalenge.participants.map(participant => {
-              const user = users.find(u => u._id.toString() === participant.userId.toString());
-  
-  
-              return {
-                  ...participant,
-                  userDetails: user || null, 
-              };
-          });
-
-          chalenge.participants = updatedParticipants;
-         
-          
-          console.log(chalenge.participants);
-          
-      }
-      chalenge.creator = creator
-
-      chalenges[i] = chalenge;
+     
+      chalenges[i] = await this.getCreatrorAndParticipants(chalenges[i] )
   }
-    
-
     return chalenges;
   }
-  getUserChalenges(userId:string){
-  return this.chalengeServiceImplimentation.getChalengeByCreator(userId)
+
+
+ async getUserChalenges(userId:string){
+
+    const chalenges =await this.chalengeServiceImplimentation.getChalengeByCreator(userId);
+
+    for (let i = 0; i < chalenges.length; i++) {
+      chalenges[i] = await this.getCreatrorAndParticipants(chalenges[i] ); 
+  }
+
+  return chalenges;
   }
   getChalengeById(id:string){
     return this.chalengeServiceImplimentation.getChalengeById(id)
   }
-  joinChalenge(id:string,userId:string):Promise<Challenge>{
-    console.log({id,userId});
+ async joinChalenge(id:string,userId:string):Promise<Challenge>{
     
-  return  this.chalengeServiceImplimentation.joinChalenge(id, userId)
+  const chalenge =await this.chalengeServiceImplimentation.joinChalenge(id, userId)
+  return await this.getCreatrorAndParticipants(chalenge)
+
+   
   }
-  createChalenge(chalenge:CreateChalengeDto):Promise<Challenge> {
-    return this.chalengeServiceImplimentation.createChalenge(chalenge);
+ async createChalenge(challenge:CreateChalengeDto):Promise<Challenge> {
+    const chalenge =await this.chalengeServiceImplimentation.createChalenge(challenge);
+    return await this.getCreatrorAndParticipants(chalenge)
   }
 
-  updateChalenge(id:string,chalenge:UpdateChalengeDto):Promise<Challenge>{
-   return this.chalengeServiceImplimentation.updateChalenge(id, chalenge)
+ async updateChalenge(id:string,challenge:UpdateChalengeDto):Promise<Challenge>{
+    const chalenge =await this.chalengeServiceImplimentation.updateChalenge(id, challenge)
+    return await this.getCreatrorAndParticipants(chalenge)
   }
   deleteChalenge(id:string,userId:string){
     return this.chalengeServiceImplimentation.deleteChalenge(id, userId)
   }
- async updateChalengesProgressByFrequency(frequency:string):Promise<Challenge[]>{
-    return await this.chalengeServiceImplimentation.updateChalengesProgressByFrequency(frequency)
+ async updateChalengesProgressByFrequency(frequency:string){
+   const challenges:Challenge[] =  await this.chalengeServiceImplimentation.updateChalengesProgressByFrequency(frequency)
+   for (const challenge of challenges) {
+   
+    if (challenge.participants && challenge.participants.length >0) {
+      
+      for(const  participant of challenge.participants){
+
+        const progress = await  this.progressService.getParticipantProgress(challenge._id as string, participant.userId as string);
+
+        if (!progress) {
+          await this.progressService.createProgress(challenge._id as string, participant.userId as string)
+        }else {
+
+        }
+      }
+    }
+
+   }
+
   }
 }
