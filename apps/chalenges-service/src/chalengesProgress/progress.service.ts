7481a1e9da173cec88b604d282ea5666e91ffe41
hissoune <khalidhissoune962@gmail.com>
2025-03-08 02:57:43 +0000

    Improved the progress tracking service by enhancing tracking logic, fixing minor inconsistencies in data handling, and improving error handling for invalid inputs.

--- apps/chalenges-service/src/chalengesProgress/progress.service.ts
+++ apps/chalenges-service/src/chalengesProgress/progress.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from "@nestjs/common";
 import { chalengeProgressImpl } from "../buisness/impl/progress-service.impl";
+import { chalengesGateway } from "../gateway/chalenges.gateway";
 
 @Injectable()
 export class ProgressService {
-    constructor(private readonly  progressImpl:chalengeProgressImpl) { }
+    constructor(private readonly  progressImpl:chalengeProgressImpl,private readonly chalengeGateway:chalengesGateway) { }
    
  
     async getParticipantProgress(chalengeId:string,userId:string){
@@ -12,5 +13,10 @@ export class ProgressService {
 
     async createProgress (userId:string,chalengeId:string){
      return this.progressImpl.createProgress(userId, chalengeId)
+    }
+
+    async updateProgress(userId:string,chalengeId:string){
+        const data = await this.progressImpl.updateProgress(userId, chalengeId)
+          this.chalengeGateway.emitchalengeUpdate(data)
     }
 }
