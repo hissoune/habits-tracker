ea2944cf8efb07dc06bf688f0ef15543d4da9840
hissoune <khalidhissoune962@gmail.com>
2025-03-17 05:31:46 +0000

    Update user.schema.ts for user schema changes

--- apps/auth-service/src/schemas/user.schema.ts
+++ apps/auth-service/src/schemas/user.schema.ts
@@ -15,6 +15,9 @@ export class User extends Document {
 
     @Prop({ required: true })
     birthDay: Date;
+    
+    @Prop({required:true,default:false})
+    isBaned:boolean
 
     @Prop({ required: true })
     image: string;
