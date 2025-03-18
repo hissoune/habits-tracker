2c8d08187382b7c9ace70a4385888454cfba7487
hissoune <khalidhissoune962@gmail.com>
2025-03-18 01:55:54 +0000

    Update notifications-service.controller.spec.ts for enhanced notification controller tests

--- apps/notifications-service/src/notifications-service.controller.spec.ts
+++ apps/notifications-service/src/notifications-service.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { NotificationsServiceController } from './notifications-service.controller';
 import { NotificationsServiceService } from './notifications-service.service';
+import { NotificationGateway } from './notifications.gateway';
 
 describe('NotificationsServiceController', () => {
   let notificationsServiceController: NotificationsServiceController;
@@ -8,7 +9,7 @@ describe('NotificationsServiceController', () => {
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [NotificationsServiceController],
-      providers: [NotificationsServiceService],
+      providers: [NotificationsServiceService,NotificationGateway],
     }).compile();
 
     notificationsServiceController = app.get<NotificationsServiceController>(NotificationsServiceController);
@@ -16,7 +17,7 @@ describe('NotificationsServiceController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(notificationsServiceController.getHello()).toBe('Hello World!');
+      expect(notificationsServiceController.checkHealth()).toBe('UP');
     });
   });
 });
