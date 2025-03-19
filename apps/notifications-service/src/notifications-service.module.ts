f945908cdc81efc5d78ac767f86cd83da88a3c92
hissoune <khalidhissoune962@gmail.com>
2025-03-19 22:02:42 +0000

    Update notifications-service.module.ts for notification module configuration

--- apps/notifications-service/src/notifications-service.module.ts
+++ apps/notifications-service/src/notifications-service.module.ts
@@ -2,10 +2,27 @@ import { Module } from '@nestjs/common';
 import { NotificationsServiceController } from './notifications-service.controller';
 import { NotificationsServiceService } from './notifications-service.service';
 import { NotificationGateway } from './notifications.gateway';
-
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PushToken, PushTokenSchema } from './schemas/notificationsPushToken';
+import notificationConfig from './config/config'
 @Module({
   imports: [
+      ConfigModule.forRoot({
+               isGlobal: true,
+               load: [notificationConfig], 
+             }),
      
+             MongooseModule.forFeature([{ name: PushToken.name, schema: PushTokenSchema }]),
+         
+             MongooseModule.forRootAsync({
+               imports: [ConfigModule],
+               inject: [ConfigService],
+               useFactory: (configService: ConfigService) => ({
+                 uri: configService.get<string>('app.mongourl'),
+                
+               }),
+             }),
   ],
   controllers: [NotificationsServiceController],
   providers: [NotificationsServiceService,NotificationGateway],
